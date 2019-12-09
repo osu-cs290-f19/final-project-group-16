b5d0c11636dbc311ec1b670d951bf1b607f5433c
Simon Wetton <simon.w3tton@gmail.com>
2019-12-08 18:58:49 -0800

    starting to add a little javascript
    finish javascript later.

--- index.js
+++ index.js
@@ -1,82 +1,49 @@
-var pictures = [,];
-var textURL = [];
+var coverURL = [];
 var genre = [];
 var album = [];
 var artist = [];
-var color;
 
-function addAlbumName(event){
-    var currText = event.currentTarget.value;
-    album = currText;
+// function addAlbumName(event){
+//     var currText = event.currentTarget.value;
+//     album = currText;
+// }
+
+// function addGenre(event){
+//     var currText = event.currentTarget.value;
+//     genre = currText;
+// }
+
+// function addURl(event){
+//     var currText = event.currentTarget.value;
+//     textURL = currText;
+// }
+
+// function addArtist(event){
+//     var currText = event.currentTarget.value;
+//     artist = currText;
+
+// }
+
+function openNav() {
+    document.getElementById('add-album').style.width = "250px";
 }
 
-function addGenre(event){
-    var currText = event.currentTarget.value;
-    genre = currText;
+function closeNav() {
+    document.getElementById('add-album').style.width = "0";
 }
 
-function addURl(event){
-    var currText = event.currentTarget.value;
-    textURL = currText;
-}
+function albumClicked(event){
 
-function addArtist(event){
-    var currText = event.currentTarget.value;
-    artist = currText;
 
 }
 
-function addPost(event){
-    var posts = document.getElementById('pictures');
-    var post = document.createElement('div');
-    post.classList.add('post');
-    post.setAttribute('data-color', color);
-    post.setAttribute('data-user',text);
-    post.setAttribute('data-url', textURL);
-    post.setAttribute('data-desc', textDesc);
-    posts.appendChild(post);
-
-    var postContents = document.createElement('img');
-    postContents.classList.add('post-pic');
-    postContents.setAttribute('src',textURL);
-    post.appendChild(postContents);
-    var clickPicture = document.getElementById('pictures').childNodes;
-    console.log(clickPicture);
-    clickPicture.forEach(function(elem){
-        elem.addEventListener('click',pictureClicked);
-    });
-
-    text = null;
-    textURL = null;
-    textDesc = null;
+function popOut(event){
+    openNav();
 }
 
-function pictureClicked(event){
-    var win = document.getElementById('picture-click');
-    var back = document.getElementById('backdrop');
-    win.classList.remove('hidden');
-    back.classList.remove('hidden');
-    console.log("clicked pictures");
-    var pic = document.getElementById('pop-up-img');
-    var url = event.currentTarget.data-url;
-    pic.setAttribute('src',event.currentTarget.dataset.url);
-}
-
-
-var textInputURL = document.getElementById('addendum-url');
-textInputURL.addEventListener('change',addURL);
-
-var textInputName = document.getElementById('addendum-name');
-textInputName.addEventListener('change',addName);
-
-var addItemButton = document.getElementById('addendum-button');
-addItemButton.addEventListener('click', addPost);
-
-var textInputDesc = document.getElementById('addendum-desc');
-textInputDesc.addEventListener('change', addDesc);
-
-var clickPicture = document.getElementById('pictures').childNodes;
-console.log(clickPicture);
-clickPicture.forEach(function(elem){
-    elem.addEventListener('click',pictureClicked);
+var hoverAdd = document.getElementById('add-album');
+hoverAdd.addEventListener('hover',popOut);
+var albums = document.getElementById('albums').childNodes;
+albums.forEach(function(elem){
+    elem.addEventListener('click',albumClicked);
 });
