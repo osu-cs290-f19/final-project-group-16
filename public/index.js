df85410f9c2007ba9b9b9059302f9355a4e2f50a
fcampos5 <fcampos5@my.chemeketa.edu>
2019-12-09 16:28:21 -0800

    Added basic server side functionality
    You can run the main page without crashing

--- public/index.js
+++ public/index.js
@@ -1,76 +1,81 @@
-var pictures = [,];
-var text = [];
-var textURL = [];
-var textDesc = [];
-var photo_name = [];
-var color;
+var coverURL = [];
+var genre = [];
+var album = [];
+var artist = [];
+
 
 function addURL(event){
     var currText = event.currentTarget.value;
-    textURL= currText;
+    coverURL = currText;
 }
 
-function addName(event){
+function addAlbumName(event){
     var currText = event.currentTarget.value;
-    text = currText;
+    album = currText;
 }
 
-function addDesc(event){
+function addArtist(event){
     var currText = event.currentTarget.value;
-    textDesc = currText;
+    artist = currText;
 }
 
-function addPost(event){
-    var posts = document.getElementById('pictures');
-    var post = document.createElement('div');
-    post.classList.add('post');
-    post.setAttribute('data-color', color);
-    post.setAttribute('data-user',text);
-    post.setAttribute('data-url', textURL);
-    post.setAttribute('data-desc', textDesc);
-    posts.appendChild(post);
-
-    var postContents = document.createElement('img');
-    postContents.classList.add('post-pic');
-    postContents.setAttribute('src',textURL);
-    post.appendChild(postContents);
-    var clickPicture = document.getElementById('pictures').childNodes;
-    console.log(clickPicture);
-    clickPicture.forEach(function(elem){
-        elem.addEventListener('click',pictureClicked);
-    });
-
-    text = null;
-    textURL = null;
-    textDesc = null;
+function addGenre(event){
+    var currText = event.currentTarget.value;
+    genre = currText;
 }
 
-function pictureClicked(event){
-    var win = document.getElementById('picture-click');
-    var back = document.getElementById('backdrop');
-    win.classList.remove('hidden');
-    back.classList.remove('hidden');
-    console.log("clicked pictures");
-    var pic = document.getElementById('pop-up-img');
-    var url = event.currentTarget.data-url;
-    pic.setAttribute('src',event.currentTarget.dataset.url);
+function addAlbum(event){
+    var albumDiv = document.createElement('div');
+    albumDiv.classList.add('album');
+    albumDiv.setAttribute('data-album',album);
+    albumDiv.setAttribute('data-artist',artist);
+    albumDiv.setAttribute('data-genre', genre);
+
+    var albumImg = document.createElement('img');
+    albumImg.src= coverURL;
+    albumImg.classList.add('album-pic');
+
+    albumDiv.appendChild(albumImg);
+
+    var songDiv = document.createElement('div');
+    songDiv.classList.add('hidden');
+
+    albumDiv.appendChild(songDiv);
+
+    var albums = document.getElementById('albums');
+    albums.appendChild(albumDiv);
+
+    var someelse = document.getElementById('add-album');
+    someelse.classList.add('hidden');
+
+    var something = document.getElementById('album-add-pic');
+    something.classList.remove('hidden');
+
+}
+
+function showAddAlbum(event){
+    var something = document.getElementById('album-add-pic');
+    something.classList.add('hidden');
+
+    var someelse = document.getElementById('add-album');
+    someelse.classList.remove('hidden');
 }
 
 
-var textInputURL = document.getElementById('addendum-url');
-textInputURL.addEventListener('change',addURL);
+var addAlbumCoverURL = document.getElementById('album-cover-add');
+addAlbumCoverURL.addEventListener('change',addURL);
 
-var textInputName = document.getElementById('addendum-name');
-textInputName.addEventListener('change',addName);
+var addAlbumNameText = document.getElementById('album-name-add');
+addAlbumNameText.addEventListener('change',addAlbumName);
 
-var addItemButton = document.getElementById('addendum-button');
-addItemButton.addEventListener('click', addPost);
+var addAlbumArtist = document.getElementById('artist-name-add');
+addAlbumArtist.addEventListener('change',addArtist);
 
-var textInputDesc = document.getElementById('addendum-desc');
-textInputDesc.addEventListener('change', addDesc);
+var addAlbumGenre = document.getElementById('genre-name-add');
+addAlbumGenre.addEventListener('change',addGenre);
 
-var clickPicture = document.getElementById('pictures').childNodes;
-console.log(clickPicture);
-clickPicture.forEach(function(elem){
-    elem.addEventListener('click',pictureClicked);
-});
+var addButton = document.getElementById('add-button');
+addButton.addEventListener('click',addAlbum);
+
+var addAlbumPic = document.getElementById('album-add-pic');
+addAlbumPic.addEventListener('click',showAddAlbum);
