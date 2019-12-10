9a91d0798da90544cd1974d328dac10be7478829
fcampos5 <fcampos5@my.chemeketa.edu>
2019-12-10 12:34:02 -0800

    Created base server

---
+++ server.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname));
+
+app.get("/", function(req,res){
+  res.status(200).render('homePage');
+});
+
+app.listen(port, function (req,res) {
+  console.log("== Server listening on port", port);
+});
